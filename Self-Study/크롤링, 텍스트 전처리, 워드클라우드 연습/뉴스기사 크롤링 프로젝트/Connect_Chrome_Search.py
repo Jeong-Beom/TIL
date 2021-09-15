10f00a11dbfb5bda6ad610617069ff0998265d51
Jeong Beom <whwjdqja58@naver.com>
2021-09-15 23:42:49 +0900

    21.09.15 0Self-Study(네이버 뉴스기사 크롤링 코드작성)

--- Self-Study/크롤링, 텍스트 전처리, 워드클라우드 연습/뉴스기사 크롤링 프로젝트/Connect_Chrome_Search.py
+++ Self-Study/크롤링, 텍스트 전처리, 워드클라우드 연습/뉴스기사 크롤링 프로젝트/Connect_Chrome_Search.py
@@ -26,4 +26,31 @@ def naver_news_page(search_words):
     driver = webdriver.Chrome(position)
     URL = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=' + quote(search_words)
     driver.get(URL)
-    
+
+def press_company_name():
+    from tqdm.notebook import tqdm
+    global press_company_list
+    press_company_list = []
+    last_num = 0
+    for i in tqdm(range(0, 400)): # 최대 페이지가 400페이지 이므로 400번 반복 > 4000개의 뉴스기사 크롤링
+        try:
+            html = driver.page_source # 해당페이지의 html 소스 전체를 크롤링 / driver앞에 붙어 있는 Chrome은 모듈 이름에 따라 변경해야합니다.
+            soup = BeautifulSoup(html, 'html.parser') # 입력되는 문자열들을 html 형태로 인식하라고 컴퓨터에 입력
+            for num in range(last_num + 50): # 1개의 웹페이지상 뉴스기사는 10개이나 html소스 sp_nws의 숫자 증가수치가 불분명하여 임의로 설정한 값입니다.
+                try:
+                    press_company_tag = f'#sp_nws{num} > div.news_wrap.api_ani_send > div > div.news_info > div.info_group > a.info.press'
+                    text = soup.select_one(press_company_tag).text
+                    text = text.replace(' 선정', '')
+                    if text[-3:] == '언론사': # 수정필요
+                        text = text[:-3]
+                    press_company_list.append(text)
+                    # print(f'sp_nws{num}은 해당페이지에 존재합니다.')
+                    last_num = num
+                except:
+                    pass
+            driver.find_element_by_xpath("//*[@id='main_pack']/div[2]/div/a[2]").click()
+            time.sleep(3)   
+        except:
+            print(f'{i+1}페이지까지 자료를 수집하였습니다.') 
+            break
+    return print(f'최종 페이지 : {i+1}', f'자료를 수집한 페이지 수 : {len(press_company_list)}', f'누락된 기사 수 : {4000 - len(press_company_list)}', sep = '\n') 
